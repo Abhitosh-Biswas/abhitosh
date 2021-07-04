1b1835dc46cb830f3ddd5432ebbc7439f5bf50be
abhitosh <abhitoshbiswas@gmail.com>
2021-07-04 13:09:25 +0530

    2nd day new commmit 3

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,7 +2,7 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world! Abhitosh.... WelCome Onboard.... new code....2nd day 2nd commit";
+    return "Hello world! Abhitosh.... WelCome Onboard.... new code...";
 
   }
 }
