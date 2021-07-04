85b2f4a9aa8c1c2ab2c1c4f50ef0af4f3aaa7884
abhitosh <abhitoshbiswas@gmail.com>
2021-07-04 11:15:35 +0530

    2nd day

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,7 +2,7 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world! Abhitosh.... WelCome Onboard.... new code";
+    return "Hello world! Abhitosh.... WelCome Onboard.... new code....2nd day";
 
   }
 }
